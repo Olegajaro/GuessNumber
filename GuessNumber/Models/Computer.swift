f0f89ea6191e3235362bd2a2e22998e04f8ff90f
Oleg Fedorov <olegajaro94@gmail.com>
2022-04-29 14:17:48 +0300

    Add logic for Game and Computer classes

--- GuessNumber/Models/Computer.swift
+++ GuessNumber/Models/Computer.swift
@@ -9,4 +9,76 @@ import Foundation
 
 final class Computer {
     
+    private(set) var computerVariant = 0
+    
+    private var computerInitallyValue = 0
+    private var left = 1
+    private var right = 100
+    
+    func initiallyGuessingNumber() -> Int {
+        computerInitallyValue = Int.random(in: left...right)
+        return computerInitallyValue
+    }
+    
+    func computerGuessingNumber(numberToGuess: Int) -> Int {
+        if computerVariant == 0 {
+            computerVariant = computerInitallyValue
+        }
+        
+        if numberToGuess > computerVariant {
+            if (right - 1) == left {
+                right = computerVariant + 1
+            }
+            
+            if right <= computerVariant {
+                right = computerInitallyValue
+                computerInitallyValue = computerVariant
+            }
+            
+            left = computerVariant
+            let listNumber = Array((computerVariant + 1)...right)
+            computerVariant = search(listNumber) ?? 0
+            left = computerVariant
+        } else if numberToGuess < computerVariant {
+            if left >= computerVariant {
+                left = computerInitallyValue
+                computerInitallyValue = computerVariant
+            }
+            
+            right = computerVariant
+            let listNumber = Array(left...(computerVariant - 1))
+            computerVariant = search(listNumber) ?? 0
+            right = computerVariant
+        } else {
+            return computerVariant
+        }
+        
+        return computerVariant
+    }
+    
+    func guessTheNumber() -> Int {
+        let setNumber = Int.random(in: 1...100)
+        
+        return setNumber
+    }
+    
+    private func search(_ array: [Int]) -> Int? {
+        
+        guard !array.isEmpty else {
+            return nil
+        }
+        
+        let left: Int = 0
+        let right: Int = array.count - 1
+        
+        if left <= right {
+            let middleIndex = (left + right) / 2
+            let middleValue = array[middleIndex]
+            
+            return middleValue
+        }
+        
+        return nil
+    }
+    
 }
--- GuessNumber/Models/Game.swift
+++ GuessNumber/Models/Game.swift
@@ -9,4 +9,56 @@ import Foundation
 
 final class Game {
     
+    let computer = Computer()
+    private var numberToGuess: Int?
+    
+    private(set) var computerTriesCount = 1
+    private(set) var playerTriesCount = 1
+    
+    init(numberToGuess: Int? = nil) {
+        self.numberToGuess = numberToGuess
+    }
+    
+    func computerFirstGuess() -> Int {
+        computer.initiallyGuessingNumber()
+    }
+    
+    func computerGuessingTheNumber() -> Int {
+        guard let numberToGuess = numberToGuess else { return 0 }
+
+        if numberToGuess != computer.computerVariant {
+            computerTriesCount += 1
+        }
+        
+        return computer.computerGuessingNumber(numberToGuess: numberToGuess)
+    }
+    
+    func computerSetsTheNumber() -> Int {
+        numberToGuess = computer.guessTheNumber()
+        return numberToGuess ?? 0
+    }
+    
+    func playerTriesToGuessTheNumber(numberToGuess: Int, playerNumber: Int) -> String {
+        if numberToGuess != playerNumber {
+            playerTriesCount += 1
+        }
+        
+        if numberToGuess > playerNumber {
+            return "No, my number is more than yours"
+        } else if numberToGuess < playerNumber {
+            return "No, my number is less than yours"
+        }
+    
+        return " "
+    }
+    
+    func getResult() -> String {
+        if computerTriesCount > playerTriesCount {
+            return "You win"
+        } else if computerTriesCount < playerTriesCount {
+            return "Computer win"
+        } else {
+            return "Draw"
+        }
+    }
 }
